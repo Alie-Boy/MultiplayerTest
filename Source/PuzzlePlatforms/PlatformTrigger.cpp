bf2e30b30219df49e3998bcb69e1dd675e023dc9
CyberBlast <ashishdantesparda@gmail.com>
2019-05-25 14:38:36 +0530

    PP008: Handling Overlap events in C++ Overlap Events weren't working in Constructor But they're working in BeginPlay

--- Source/PuzzlePlatforms/PlatformTrigger.cpp
+++ Source/PuzzlePlatforms/PlatformTrigger.cpp
@@ -15,13 +15,19 @@ APlatformTrigger::APlatformTrigger()
 	if (!ensure(TriggerVolume)) return;
 
 	RootComponent = TriggerVolume;
+	TriggerVolume->SetGenerateOverlapEvents(true);
 }
 
 // Called when the game starts or when spawned
 void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
-
+	if (HasAuthority())
+	{
+		//SetReplicates(true);
+		TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnBeginOverlap);
+		TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnEndOverlap);
+	}
 }
 
 // Called every frame
@@ -29,5 +35,17 @@ void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void APlatformTrigger::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated!"));
+}
+
+void APlatformTrigger::OnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
 }
 
--- Source/PuzzlePlatforms/PlatformTrigger.h
+++ Source/PuzzlePlatforms/PlatformTrigger.h
@@ -26,6 +26,13 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+						int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+						int32 OtherBodyIndex);
+
 private:
 	UPROPERTY(VisibleAnywhere)
 	UBoxComponent* TriggerVolume = nullptr;
